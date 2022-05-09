5c1df1e4660dcd004833e497b1178d592f20c4b6
Juan Carlos Ruperto Zarate <juancarloszarate586@gmail.com>
2022-05-09 09:50:03 -0500

    Se agrego el boton de borrar de la lista y bloquear los botones

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 
 function App() {
-
   const [inputState, setInputState] = useState({
     titulo: "",
     fecha: "",
@@ -29,51 +28,55 @@ function App() {
   };
 
   const handleClickGuardar = () => {
-    setNotas([...notas, inputState])
+    setNotas([...notas, inputState]);
     localStorage.setItem("notas", JSON.stringify(notas));
     handleResetChange();
   };
-
 
   const handleBorrarNota = (index) => {
     const nuevoArreglo = [];
 
     notas.forEach((nota, i) => {
-      if (index !== i){
+      if (index !== i) {
         nuevoArreglo.push(nota);
       }
     });
     localStorage.setItem("notas", JSON.stringify(nuevoArreglo));
     setNotas([...nuevoArreglo]);
+  };
 
+  const handleClickLimpiaLista = () => {
+   setNotas([]);
+   localStorage.setItem("notas", JSON.stringify([]));
+  
   };
 
   return (
     <div className=" App container mt-4">
       <div className="row  ">
-        <div className="row card shadow"> 
+        <div className="row card shadow" id="contenedor">
           <div className="col p-4">
-            <h3 className="text-center bi bi-card-list">Lista</h3>
+            <h3 className="text-center bi bi-card-list"> Lista</h3>
 
-            {notas.length === 0 ?
+            {notas.length === 0 ? (
               "Al momento no tienes notas guardadas. Puedes crear una en el formulario contiguo"
-            :
-            (
+            ) : (
               <ol>
-                {notas.map((item, index)=>{
+                {notas.map((item, index) => {
                   return (
                     <li key={index}>
                       {item.titulo} ({item.fecha}) {item.nota} &nbsp;
-                      <i className="bi bi-x-circle-fill"
-                      onClick={() => handleBorrarNota(index)}
-                      style={{color: "red", cursor:"pointer"}}></i>
+                      <i
+                        className="bi bi-x-circle-fill "
+                        onClick={() => handleBorrarNota(index)}
+                        style={{ color: "red", cursor: "pointer" }}
+                      ></i>
                     </li>
-                    
-                  )
+                  );
                 })}
               </ol>
-            )
-            }
+            )}
+
             {/* {arregloNotas.length !== 0 &&(
               <ol>
                 {arregloNotas.map((item)=>{
@@ -86,11 +89,29 @@ function App() {
               </ol>
             )} */}
           </div>
+          {/* Boton agregado por mi */}
+          <hr />
+          <div className="text-center">
+            <button
+              type="button"
+              className="btn btn-outline-secondary mx-2 "
+              disabled={notas.length===0}
+              onClick={handleClickLimpiaLista}
+              style={{ marginLeft: "15px", width: "250px" }}
+            >
+              <i class="bi bi-trash-fill" style={{ color: "red" }}></i> Borrar
+            </button>
+          </div>
+
+          <br />
+
           <div className="col text-center mx-auto p-4">
-            <h3 className="bi bi-file-earmark-plus-fill">Notas</h3>
+            <h3 className="bi bi-file-earmark-plus-fill"> Notas</h3>
             <label className="mb-2" style={{ width: "100%" }}>
-              Titulo
+              {" "}
+              <i class="bi bi-chat-square-text-fill"></i> Titulo
               <input
+                className="text-center"
                 id="titulo"
                 name="titulo"
                 type="text"
@@ -101,8 +122,10 @@ function App() {
             </label>
             <br></br> <br />
             <label className="mb-2  " style={{ width: "100%" }}>
-              Fecha
+              {" "}
+              <i class="bi bi-calendar-date"></i> Fecha
               <input
+                className="text-center"
                 id="fecha"
                 name="fecha"
                 type="date"
@@ -114,7 +137,8 @@ function App() {
             <br></br>
             <br />
             <label style={{ width: "100%" }}>
-              Nota
+              {" "}
+              <i class="bi bi-card-text"></i> Nota
               <textarea
                 id="nota"
                 name="nota"
@@ -129,12 +153,16 @@ function App() {
                 <span className="row mx-1">
                   <button
                     type="button"
-                    className="btn btn-primary mx-2 "
+                    className="btn btn-outline-secondary mx-2 "
+                    disabled={inputState.titulo==="" || 
+                    inputState.fecha==="" || 
+                    inputState.titulo==="" }
                     onClick={handleResetChange}
                     style={{ marginLeft: "15px" }}
+                    
                   >
-                    <i class="bi bi-trash-fill" style={{color: "red"}}></i>
-                     Borrar
+                    <i class="bi bi-trash-fill" style={{ color: "red" }}></i>{" "}
+                    Borrar
                   </button>
                 </span>
               </div>
@@ -143,11 +171,23 @@ function App() {
                 <span className="row mx-1">
                   <button
                     type="button"
-                    className="btn btn-primary mx-2 "
+                    className="btn btn-outline-secondary mx-2 "
+                    disabled={inputState.titulo==="" || 
+                    inputState.fecha==="" || 
+                    inputState.titulo===""}
                     onClick={handleClickGuardar}
                     style={{ marginLeft: "15px" }}
+                    
                   >
-                   <i class="bi bi-save-fill"style={{color: "red", fontSize:"0.75rem", cursor:"pointer"}}></i>  Guardar
+                    <i
+                      class="bi bi-save-fill"
+                      style={{
+                        color: "red",
+                        fontSize: "0.75rem",
+                        cursor: "pointer",
+                      }}
+                    ></i>{" "}
+                    Guardar
                   </button>
                 </span>
               </div>
@@ -163,6 +203,7 @@ function App() {
             width={550}
             alt=""
           />
+       
         </center>
         <center>
           <br /> <hr />
